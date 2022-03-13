e211760d5a837f6f5b0ed67f4a68931b53149fbe
Vsevolod Bazhan <vsevozhan@yandex.ru>
2022-03-13 22:32:11 +1000

    feat(scripts/init.sql): create `exchange_rates` in `public` schema

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS dictionary;
-
-CREATE TABLE IF NOT EXISTS dictionary.exchange_rates (
+CREATE TABLE IF NOT EXISTS exchange_rates (
     "from" CHAR(3) NOT NULL,
     "to" CHAR(3) NOT NULL,
     "date" DATE NOT NULL,
